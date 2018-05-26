a6aab7da1893bd1316604f2cbebeb2749ae16534
Michael Sweeney <michael.d.sweeney@prudential.com>
2018-05-26 07:07:07 -0700

    Added LYIT Java file for Project L00064451

---
+++ L00064451.java
@@ -0,0 +1,6 @@
+public class HelloLYIT {
+ public static void main(String[] args) {
+ // Prints "Hello, LYIT from L00064451" to the terminal window.
+ System.out.println("Hello, LYIT from L00064451");
+ }
+}
